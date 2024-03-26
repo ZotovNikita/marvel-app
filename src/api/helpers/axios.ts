eb82b8fa71a50bbc5b1cad58158c63beffd46797
ZotovNikita <usernamex821@gmail.com>
2024-03-26 19:04:59 +0600

    feat: add toast

--- src/api/helpers/axios.ts
+++ src/api/helpers/axios.ts
@@ -1,10 +1,29 @@
 import axios from 'axios';
-
+import { Flip, toast } from 'react-toastify';
 // Config
 import envs from '../../config/environments';
 
 const instance = axios.create({
   baseURL: envs.baseApiUrl
 });
+
+instance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    toast.error(error.response.data.message, {
+      position: 'top-right',
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: false,
+      draggable: true,
+      theme: 'light',
+      transition: Flip
+    });
+    return Promise.reject(error);
+  }
+);
 
 export default instance;
